190fb83f8ad428671dd4cd3fba33516652b1cc6c
Bibhu <100408110+DeBibhu562@users.noreply.github.com>
2023-08-09 10:19:43 +0530

    banner crousal color

--- src/Components/carousel.jsx
+++ src/Components/carousel.jsx
@@ -9,22 +9,22 @@ export const Carousel = () => {
           <li data-bs-target="#carouselExampleDark" data-bs-slide-to="2"></li>
         </ol>
         <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="3000">
-            <img src="images/offer-slider1.png" className="d-block w-100" alt="..."/>
+          <div className="carousel-item active" data-bs-interval="3000" >
+            <img src="images/offer-slider1.png" style={{maxHeight:"400px"}} className="d-block w-100" alt="..."/>
             <div className="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
               <p>Sale sale sale</p>
             </div>
           </div>
           <div className="carousel-item" data-bs-interval="3000">
-            <img src="images/offer-slider1.png" className="d-block w-100" alt="..."/>
+            <img src="images/offer-slider2.jpg" style={{maxHeight:"400px"}} className="d-block w-100" alt="..."/>
             <div className="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
               <p>Damaka offer</p>
             </div>
           </div>
           <div className="carousel-item" data-bs-interval="3000">
-            <img src="images/offer-slider1.png" className="d-block w-100" alt="..."/>
+            <img src="images/offer-slider3.jfif" style={{maxHeight:"400px"}} className="d-block w-100" alt="..."/>
             <div className="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
               <p>Grab It Fast.</p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Carousel } from "../Components/carousel";
+import ProductList from "../Components/ProductList";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {Banner} from "../Components/banner";
+import { GridCards } from "../Components/GridCard";
+import { MdBanners } from "../Components/MdBanner";
 
 export const Home = () => {
+  // const dispatch = useDispatch();
+
+  // const Products = useSelector(state => state.product.products)
+
+  // useEffect(()=>{
+  //   dispatch(initializeProductsAC());
+  // },[])
+
+
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Carousel />
+      <Banner img_link="banner1"/>
+      <Banner img_link="banner2"/>
+      <Banner img_link="banner3"/>
+      <GridCards/>
+      <MdBanners/>
+      <ProductList />
+    </>
+  );
+};
