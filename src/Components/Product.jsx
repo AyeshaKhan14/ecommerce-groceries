690963f6b2446391e4e69160cf7203af1fd3d9a0
Ayeshakhan14 <ayeshatravels9@gmail.com>
2023-08-11 19:39:12 +0530

    overflow solve

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link} from "react-router-dom";
 
 export default function Product(props) {
-  console.log(props,"prk")
   let desc = "";
   if (props.description && props.description.length > 20) {
     desc = props.el.description.substr(0, 20) + " ...";
@@ -16,7 +15,7 @@ export default function Product(props) {
        <img className="product--image" src={props.el.image} alt="product image" />
       <h5>{props.el.name}</h5>
       <p className="price" style={{color:"gray",fontSize:"16px",fontStyle:"italic",fontWeight:"bold" }}>Price: ${props.el.price}</p>
-      <p >{desc}</p>
+      <p style={{maxWidth:"100%",overflow:"hidden", textoverflow:"ellipsis", whiteSpace:"nowrap"}}>{desc}</p>
       </Link>
     </div>
   );
